222a1aa5bbfbbb42afbf50e145f236ddf20f1398
HwanSu-K <kuma2301@naver.com>
2021-12-05 23:38:50 +0900

    랭킹 조회 및 등록 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { Progress, Button } from 'antd';
+import { Modal, Progress, Button, Typography, Space, Input } from 'antd';
 import axios from 'axios';
 import './App.css';
 import 'antd/dist/antd.css';
 
+const { Text } = Typography;
 const testSize = 5;
 
 const App = () => {
@@ -11,6 +12,53 @@ const App = () => {
   const [message, setMessage] = useState('시작');
   const [result, setResult] = useState([]);
   const [rankList, setRankList] = useState([]);
+  const [visible, setVisible] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [name, setName] = useState('');
+
+  const onCheangeName = (e) => {
+    setName(e.target.value);
+  };
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleOk = () => {
+    setConfirmLoading(true);
+    setTimeout(() => {
+      axios.post('http://developer-green-final.click:9000/members', {
+        memberName: name,
+        rateTime: parseInt(result.reduce((a, c) => a + c) / result.length),
+      });
+
+      setName('');
+      setVisible(false);
+      setConfirmLoading(false);
+      setResult([]);
+      setState('waiting');
+      setMessage('시작');
+    }, 2000);
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+  };
+
+  function info() {
+    Modal.info({
+      title: '전체 랭킹',
+      content: (
+        <>
+          {rankList.map((v, i) => (
+            <div key={i}>
+              {i + 1}등 : {v.memberName}({v.rateTime}ms)
+            </div>
+          ))}
+        </>
+      ),
+      onOk() {},
+    });
+  }
 
   const timeout = useRef(null);
   const startTime = useRef();
@@ -41,6 +89,7 @@ const App = () => {
       }
     }
   };
+
   const screenNow = () => {
     timeout.current = setTimeout(() => {
       setState('now');
@@ -48,13 +97,14 @@ const App = () => {
       startTime.current = new Date();
     }, Math.floor(Math.random() * 2000) + 3000);
   };
+
   const renderScoreList = () => {
     return result.length === 0 ? null : (
       <>
         {result.map((v, i) => (
-          <div key={i}>
+          <Text key={i}>
             {i + 1}회차 : {v}ms
-          </div>
+          </Text>
         ))}
       </>
     );
@@ -62,13 +112,12 @@ const App = () => {
 
   useEffect(() => {
     axios
-    .get('http://127.0.0.1:8080/users/total-rank')
-    .then((res) => {
-      console.log(res.data);
-      setRankList(res.data);
-    })
-    .catch((err) => console.error(err));
-  }, [])
+      .get('http://developer-green-final.click:9000/members/rank')
+      .then((res) => {
+        setRankList(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, [visible]);
 
   return (
     <>
@@ -78,25 +127,26 @@ const App = () => {
           <span id="message">
             {result.length === testSize ? (
               <>
-                최고기록 : {Math.min(...result)}ms
+                <Text type="secondary">평균기록 : {parseInt(result.reduce((a, c) => a + c) / result.length)}ms</Text>
                 <br />
-                <Button>등록</Button>
+                <Button type="primary" onClick={showModal}>
+                  등록
+                </Button>
               </>
             ) : (
               message
             )}
           </span>
         </div>
-        <div id="result">{renderScoreList()}</div>
-              
-        {rankList.map((v, i) => (
-          <>
-            <div key={i}>
-              {i + 1}등 : {v.name}({v.ratetime}ms)
-            </div>
-          </>
-        ))}
+
+        <Button style={{ marginBottom: 30 }} onClick={info}>
+          랭킹 확인
+        </Button>
+        <Space direction="vertical">{renderScoreList()}</Space>
       </div>
+      <Modal title="랭킹 등록" visible={visible} onOk={handleOk} confirmLoading={confirmLoading} onCancel={handleCancel}>
+        <Input placeholder="닉네임을 입력하세요" value={name} onChange={onCheangeName} />
+      </Modal>
     </>
   );
 };
